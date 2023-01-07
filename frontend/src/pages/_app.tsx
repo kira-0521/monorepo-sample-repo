2a77ed0611447097d71ba1ff1e8b71c04fca34d3
kira-0521 <kiratanaka0521@gmail.com>
2023-01-07 22:22:42 +0900

    test

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@emotion/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { CssBaseline } from '@mui/material'
+import theme from '../theme'
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
+
+export default App
