882000aa85a65fc7bb2694966b6e0963e5ebdd91
kira-0521 <kiratanaka0521@gmail.com>
2023-01-08 16:15:02 +0900

    feat: mui theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,5 +1,17 @@
 import { createTheme } from '@mui/material'
 
-const theme = createTheme({})
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#007BFF',
+    },
+    secondary: {
+      main: '#6C757D',
+    },
+    warning: {
+      main: '#FFC107',
+    },
+  },
+})
 
 export default theme
