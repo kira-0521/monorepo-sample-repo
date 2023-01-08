eb09dad3d2134019b4614b4e5921d6de20cf9121
kira-0521 <kiratanaka0521@gmail.com>
2023-01-08 16:14:00 +0900

    ♻️

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@emotion/react'
-
 import { CssBaseline } from '@mui/material'
+
 import theme from '../theme'
 
 const App = ({ Component, pageProps }: AppProps) => (
