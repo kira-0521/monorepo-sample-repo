a8cd937a63db4e2792cd29a12f5efaa15de59021
kira-0521 <kiratanaka0521@gmail.com>
2023-01-09 17:22:23 +0900

    test

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,38 @@
-import { useCallback, useState } from 'react'
+import { ChangeEvent, FormEventHandler, useCallback, useState } from 'react'
 
-import { OverrideTypography, PrimaryButton } from '../components/atoms'
+import { MyForm } from '@/components/molecules/Forms'
+import { MyInput } from '@/components/atoms/Forms/MyInput'
+import { Box, FormControl, Input } from '@mui/material'
 
 const Home = () => {
-  const [state, setState] = useState(false)
-  const handleClick = useCallback(() => setState((s) => !s), [setState])
+  const [formStates, setFormStates] = useState({
+    email: '',
+    password: '',
+  })
+
+  const handleChange = useCallback(
+    (formLabel: 'email' | 'password') => (e: ChangeEvent<HTMLInputElement>) => {
+      const label = formLabel
+      setFormStates((form) => ({ ...form, [label]: e.target.value }))
+    },
+    [setFormStates]
+  )
+
+  const handleSubmit = useCallback<FormEventHandler<HTMLFormElement>>((e) => {
+    e.preventDefault()
+  }, [])
   return (
-    <div>
-      <PrimaryButton onClick={handleClick}>Children Desu</PrimaryButton>
-      {state && <OverrideTypography variant="h1">Hello</OverrideTypography>}
-    </div>
+    <Box p="30px">
+      <MyForm onSubmit={handleSubmit}>
+        <FormControl>
+          <MyInput label="email" onChange={handleChange('email')} />
+        </FormControl>
+        <FormControl>
+          <MyInput label="password" onChange={handleChange('password')} />
+        </FormControl>
+        <Input type="submit" value="submit" />
+      </MyForm>
+    </Box>
   )
 }
 
