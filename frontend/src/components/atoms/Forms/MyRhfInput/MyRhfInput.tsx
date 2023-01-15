22060196f7968f95a555e7ce042987e3db46f5ca
kira-0521 <kiratanaka0521@gmail.com>
2023-01-15 15:54:12 +0900

    feat: emotion

--- frontend/src/components/atoms/Forms/MyRhfInput/MyRhfInput.tsx
+++ frontend/src/components/atoms/Forms/MyRhfInput/MyRhfInput.tsx
@@ -1,12 +1,18 @@
+import { css } from '@emotion/react'
 import { memo, FC, HTMLAttributes } from 'react'
 import { UseFormRegisterReturn } from 'react-hook-form'
+
+const style = css({
+  border: '3px solid #000',
+  borderRadius: '5px',
+})
 
 interface MyRhfInputProps extends HTMLAttributes<HTMLInputElement> {
   register: UseFormRegisterReturn
 }
 
 export const MyRhfInput: FC<MyRhfInputProps> = memo(
-  ({ register, ...props }) => <input {...register} {...props} />
+  ({ register, ...props }) => <input {...register} {...props} css={style} />
 )
 
 MyRhfInput.displayName = 'MyRhfInput'
